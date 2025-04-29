5d17589434d99cfb708d1b641ef7313f9fa9422b
Jason Golden <jasonwilliamgolden@gmail.com>
2025-04-29 18:17:38 -0400

    Fix SQL script by adding UUID extension

--- create_user_registrations_table.sql
+++ create_user_registrations_table.sql
@@ -1,3 +1,6 @@
+-- Enable UUID extension if not already enabled
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
 -- Create user_registrations table for storing sign-up information
 CREATE TABLE IF NOT EXISTS user_registrations (
   id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
